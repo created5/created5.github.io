e720cdc21030fa838b21590d1dbf8add1db49158
Athan <athanpapas7@gmail.com>
2017-07-10 19:33:58 +0000

    right format

---
+++ index.js
@@ -0,0 +1,4 @@
+<link rel="stylesheet" href="stylesheet.css" type="text/css"></link>;
+/*global $*/$(/*global button*/button).on('click', function() {
+    (this).toggleClass("dropdown-content");
+});
